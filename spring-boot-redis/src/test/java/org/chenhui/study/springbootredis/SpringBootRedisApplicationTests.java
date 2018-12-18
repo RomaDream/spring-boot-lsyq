a88f73ebaf1b38468937dcda5929b40cf2775290
chenhui <947840093@qq.com>
2018-12-18 17:41:58 +0800

    spring boot redis集成
    Signed-off-by: chenhui <947840093@qq.com>

---
+++ spring-boot-redis/src/test/java/org/chenhui/study/springbootredis/SpringBootRedisApplicationTests.java
@@ -0,0 +1,25 @@
+package org.chenhui.study.springbootredis;
+
+import org.chenhui.study.springbootredis.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRedisApplicationTests {
+	@Autowired
+	private RedisTemplate<Object, Object> redisTemplate;
+	@Test
+	public void contextLoads() {
+		User user = new User(2l, "张san");
+		redisTemplate.opsForValue().set("user", user);
+		User user1 = (User) redisTemplate.opsForValue().get("user");
+		System.out.println(user1);
+	}
+
+}
+
