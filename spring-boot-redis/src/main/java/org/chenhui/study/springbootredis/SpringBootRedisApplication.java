1751aa0519af031326c0cef27bb9e969234f7fe8
chenhui <947840093@qq.com>
2018-12-20 11:02:59 +0800

    定时发送消息到频道

--- spring-boot-redis/src/main/java/org/chenhui/study/springbootredis/SpringBootRedisApplication.java
+++ spring-boot-redis/src/main/java/org/chenhui/study/springbootredis/SpringBootRedisApplication.java
@@ -1,14 +1,27 @@
 package org.chenhui.study.springbootredis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootRedisApplication {
 
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRedisApplication.class, args);
 	}
-
+	
+	@Scheduled(fixedDelay = 2000)
+	public void sendTask(){
+		stringRedisTemplate.convertAndSend("message-test", "this is a message from sendTask");
+	}
+	
 }
 
